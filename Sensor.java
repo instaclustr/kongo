04d1afc01dd2a89146711185bfab82882e38bf9e
paul-brebner <31493691+paul-brebner@users.noreply.github.com>
2018-03-13 14:24:46 +1100

    Add files via upload

---
+++ Sensor.java
@@ -0,0 +1,27 @@
+package com.instaclustr.kongo;
+
+/*
+ * Sensor objects are for warehouse and truck environmental metric values.
+ */
+
+public class Sensor {  
+
+	long time;
+	String doc;
+	String tag;
+	String metric;
+	double value;
+
+	public Sensor(long time, String doc, String tag, String metric, double value) {
+		this.time = time;
+		this.doc = doc;
+		this.tag = tag;
+		this.metric = metric;
+		this.value = value;
+	}
+	
+	public void print()
+	{
+		System.out.println(time + ", " + doc + ", " + tag + ", " + metric + "=" + value);
+	}
+}
